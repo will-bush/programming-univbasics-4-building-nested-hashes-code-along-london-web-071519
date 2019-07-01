73e1e0361a0f0cf36083a5d63e56cc23b5777ad1
Will Bush <williamdcbush@gmail.com>
2019-07-01 15:35:38 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,11 +2,18 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {}
-      
+      :matriarch => {}
+      :hero => {}
+      hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
+      :patriarch => {}
+      :matriarch => {}
+      :heroine => {}
+      heroine_friends => []
+      }
+    
+  }
 
   
 end
