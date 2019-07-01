0bfd3ee2633fa8c37692b196650edb2d3fc550f4
Will Bush <williamdcbush@gmail.com>
2019-07-01 15:37:15 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,12 +4,12 @@ def second_challenge
       :patriarch => {}
       :matriarch => {}
       :hero => {}
-      hero_friends => []
+      :hero_friends => []
       }, 
     :capulet => {
       :patriarch => {}
       :matriarch => {}
       :heroine => {}
-      heroine_friends => []
+      :heroine_friends => []
       }}
 end
